df70595e11b693ccf754640e8851a23f2c851a46
Alejandro Goez <alejandro.goez@egm.local>
2022-09-01 12:24:31 -0500

    feature:integration_templete

---
+++ resources/views/layouts/theme/sidebar.blade.php
@@ -0,0 +1,93 @@
+<div class="sidebar-wrapper sidebar-theme">
+
+    <nav id="compactSidebar">
+        <ul class="menu-categories">
+            <li class="active">
+                <a href="{{url('categories')}}" class="menu-toggle" data-active="true">
+                    <div class="base-menu">
+                        <div class="base-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="
+                                feather feather-grid"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width=
+                                "7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width=
+                                "7" height="7"></rect>
+                            </svg>
+                        </div>
+                        <span>CATEGORIAS</span>
+                    </div>
+                </a>
+            </li>
+
+            <li class="">
+                <a href="{{url('products')}}" class="menu-toggle" data-active="false">
+                    <div class="base-menu">
+                        <div class="base-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="feather feather-box"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
+                                <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line>
+                            </svg>
+                        </div>
+                        <span>PRODUCTOS</span>
+                    </div>
+                </a>
+            </li>
+
+            <li class="">
+                <a href="{{url('roles')}}" class="menu-toggle" data-active="false">
+                    <div class="base-menu">
+                        <div class="base-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
+                                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                                <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                            </svg>
+                        </div>
+                        <span>ROLES</span>
+                    </div>
+                </a>
+            </li>
+
+            <li class="">
+                <a href="{{url('permissions')}}" class="menu-toggle" data-active="false">
+                    <div class="base-menu">
+                        <div class="base-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                                <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                            </svg>
+                        </div>
+                        <span>PERMISOS</span>
+                    </div>
+                </a>
+            </li>
+
+            <li class="">
+                <a href="{{url('assing')}}" class="menu-toggle" data-active="false">
+                    <div class="base-menu">
+                        <div class="base-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle>
+                            </svg>
+                        </div>
+                        <span>ASIGNAR</span>
+                    </div>
+                </a>
+            </li>
+
+            <li class="">
+                <a href="{{url('users')}}" class="menu-toggle" data-active="false">
+                    <div class="base-menu">
+                        <div class="base-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle>
+                            </svg>
+                        </div>
+                        <span>USUARIOS</span>
+                    </div>
+                </a>
+            </li>
+    </nav>
+</div>
+<div id="compact_submenuSidebar" class="submenu-sidebar" style="display: none!important">
+</div>
